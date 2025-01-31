f744a55eaea03701f406aca128e6f3781a8c3197
Rahil1910 <rahilrafique1234@gmail.com>
2025-01-31 16:07:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-//your JS code here. If required.
+const submit = document.getElementById('submit');
+        const check = document.getElementById('checkbox');
+        submit.addEventListener('click',()=>{
+            const user = document.getElementById('username').value;
+            const pass = document.getElementById('password').value;
+            const data = localStorage.getItem(user);
+            if(check.checked){
+                localStorage.setItem("username",user);
+                localStorage.setItem("password",pass);
+                alert(`Logged in as ${user}`);
+            }
+            if(!check.checked){
+                localStorage.removeItem("username");
+                localStorage.removeItem("password");
+                alert("data is removed");
+            }
+        })
+        function input(){
+            const user = document.getElementById('username').value;
+            const pass = document.getElementById('password').value;
+            const data = localStorage.getItem("username");
+            if(data){
+                const existing = document.createElement('button');
+                existing.id="exist";
+                existing.textContent = "Login as existing user";
+                existing.addEventListener('click',()=>{
+                    alert(`Logged in as ${user}`);
+                })
+                const form = document.getElementById('form');
+                form.appendChild(existing);
+            }
+        }
